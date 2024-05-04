96d346cc6e29d68372cb1ebecc4ee8d0258a7a88
ervanherdiansyah <77282241+ervanherdiansyah@users.noreply.github.com>
2024-05-04 19:48:13 +0700

    update

--- routes/api.php
+++ routes/api.php
@@ -30,21 +30,19 @@ Route::group([
     Route::post('me', [AuthController::class, 'me']);
 });
 
-Route::group(function () {
-    // Produk
-    Route::get('/produk', [ProductController::class, 'getProduct']);
-    Route::post('/produk/create', [ProductController::class, 'createProduct']);
-    Route::post('/produk-byid/{id}', [ProductController::class, 'getProductById']);
-    Route::post('/produk/update/{id}', [ProductController::class, 'updateProduct']);
-    Route::delete('/produk/delete/{id}', [ProductController::class, 'deleteProduct']);
+// Produk
+Route::get('/produk', [ProductController::class, 'getProduct']);
+Route::post('/produk/create', [ProductController::class, 'createProduct']);
+Route::post('/produk-byid/{id}', [ProductController::class, 'getProductById']);
+Route::post('/produk/update/{id}', [ProductController::class, 'updateProduct']);
+Route::delete('/produk/delete/{id}', [ProductController::class, 'deleteProduct']);
 
-    // Paket
-    Route::get('/paket', [PaketController::class, 'getPaket']);
-    Route::post('/paket/create', [PaketController::class, 'createPaket']);
-    Route::post('/paket-byid/{id}', [PaketController::class, 'getPaketById']);
-    Route::post('/paket/update/{id}', [PaketController::class, 'updatePaket']);
-    Route::delete('/paket/delete/{id}', [PaketController::class, 'deletePaket']);
-});
+// Paket
+Route::get('/paket', [PaketController::class, 'getPaket']);
+Route::post('/paket/create', [PaketController::class, 'createPaket']);
+Route::post('/paket-byid/{id}', [PaketController::class, 'getPaketById']);
+Route::post('/paket/update/{id}', [PaketController::class, 'updatePaket']);
+Route::delete('/paket/delete/{id}', [PaketController::class, 'deletePaket']);
 
 Route::put('updateUserDetails/{id}', [UserDetailController::class, 'updateUserDetailAdmin']);
 Route::get('alluserdetails', [UserDetailController::class, 'getUserDetails']);
