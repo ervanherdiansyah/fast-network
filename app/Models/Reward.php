2baab0eed8a3e4b8b4e19bf925e6fa1f979b23a5
akbarhadiq <akbarhadiq.ha@gmail.com>
2024-05-07 13:10:53 +0700

    Feat : Update Model Reward,Withdraw

--- app/Models/Reward.php
+++ app/Models/Reward.php
@@ -8,4 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Reward extends Model
 {
     use HasFactory;
+    protected $guarded = [
+        'id',
+    ];
 }
--- app/Models/WithdrawPoint.php
+++ app/Models/WithdrawPoint.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class WithdrawPoint extends Model
 {
     use HasFactory;
+    protected $guarded = [
+        'id',
+    ];
+    public function users(){
+        return $this->belongsTo(User::class, "user_id");
+        
+    }
+
+    public function rewards(){
+        return $this->belongsTo(Reward::class, "reward_id");
+    }
 }
