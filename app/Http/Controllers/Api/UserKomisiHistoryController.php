e4e2ada79663ee6d440ba5180f2813a300ad77cd
akbarhadiq <akbarhadiq.ha@gmail.com>
2024-05-22 17:59:08 +0700

    Fix : Fix Model Poin History Affiliator

--- app/Http/Controllers/Api/UserKomisiHistoryController.php
+++ app/Http/Controllers/Api/UserKomisiHistoryController.php
@@ -13,7 +13,7 @@ class UserKomisiHistoryController extends Controller
     public function getKomisiHistory(Request $request){
         try{
             $user_id = Auth::user()->id;
-            $userKomisiHistory = UserKomisiHistory::where('affiliate_id', $user_id)->get();
+            $userKomisiHistory = UserKomisiHistory::where('affiliator_id', $user_id)->get();
             return response()->json(['data' => $userKomisiHistory, 'message' => 'Success'], 200); 
         }
         catch(\Throwable $th){
--- app/Http/Controllers/Api/UserPoinHistoryController.php
+++ app/Http/Controllers/Api/UserPoinHistoryController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\AffiliatorPoinHistory;
 use App\Models\UserPoinHistory;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -13,8 +14,19 @@ class UserPoinHistoryController extends Controller
     public function getPointHistory(Request $request){
         try{
             $user_id = Auth::user()->id;
-            $userPoinHistoryAsUser = UserPoinHistory::where('user_id', $user_id);
-            return response()->json(['data' => $userPoinHistoryAsUser, 'message' => 'Success'], 200); 
+            $userPoinHistoryAsUser = UserPoinHistory::where('user_id', $user_id)->get();
+            $userPoinHistoryAsAffiliator = AffiliatorPoinHistory::where('affiliator_id', $user_id)->get();
+            if(!$userPoinHistoryAsAffiliator){
+                $mergedUserPoinHistory = $userPoinHistoryAsUser->merge($userPoinHistoryAsAffiliator);
+                return response()->json(['data' => $mergedUserPoinHistory, 'message' => 'Success'], 200); 
+                // return response()->json(['data' => $userPoinHistoryAsUser, 'message' => 'Success'], 200); 
+            }
+            else{
+                // $mergedUserPoinHistory = $userPoinHistoryAsUser->getCollection()->merge($userPoinHistoryAsAffiliator->getCollection());
+                $mergedUserPoinHistory = $userPoinHistoryAsUser->merge($userPoinHistoryAsAffiliator);
+                return response()->json(['data' => $mergedUserPoinHistory, 'message' => 'Success'], 200); 
+            }
+            
         }
         catch(\Throwable $th){
             return response()->json(['message' => 'Internal Server Error'], 500);
